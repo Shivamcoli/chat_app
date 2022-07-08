b26d7eaddd2330673e3876753272492bc7c9b1d6
Shivamcoli <shivamkolii789@gmail.com>
2022-07-08 20:40:01 +0530

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:chatapp/components/rounded_button.dart';
+class WelcomeScreen extends StatefulWidget {
+  static const String id='welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller=AnimationController(
+        duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(controller.value);
+    });
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                    text: ['Flash Chat'],
+                    speed: Duration(milliseconds: 300),
+                    textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black45,
+                  ),
+
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              colour: Colors.lightBlueAccent,
+              title: 'Log in',
+              onPressed: (){
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+          RoundedButton(colour: Colors.blueAccent, title: 'Register', onPressed: (){
+            Navigator.pushNamed(context, RegistrationScreen.id);
+          }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
